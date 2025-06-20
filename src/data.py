6bc49dd5033120b775f29b39c8407ccf45f15a21
LeGiangK62 <tung.giangle99@gmail.com>
2025-06-20 11:21:07 +0900

    AddDataset

--- src/data.py
+++ src/data.py
@@ -1,55 +1,79 @@
 import torch
 import os
-from torch_geometric.datasets import TUDataset, Planetoid
+from torch_geometric.datasets import TUDataset, ZINC, Planetoid, WikipediaNetwork
 from torch_geometric.loader import DataLoader
 
 def load_dataset(name, path='../data', train_size=None, test_size=None, batch_size=32):
     name = name.upper()
-    
-    if name in ['MUTAG', 'ENZYMES', 'PROTEINS']:
+    task_type = 'graph'
+
+    if name in ['MUTAG', 'ENZYMES', 'PROTEINS', 'REDDIT-BINARY']:
         dataset = TUDataset(os.path.join(path, 'TUDataset'), name=name)
         torch.manual_seed(1712)
         dataset = dataset.shuffle()
-        if train_size and test_size:
-            train_dataset = dataset[:train_size]
-            test_dataset = dataset[train_size:train_size + test_size]
-        else:
-            train_dataset = dataset[:int(0.8 * len(dataset))]
-            test_dataset = dataset[int(0.8 * len(dataset)):]
-        train_loader = DataLoader(train_dataset, batch_size=batch_size)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size)
-        task_type = 'graph'
+
+    elif name == 'ZINC':
+        dataset = ZINC(root=os.path.join(path, 'ZINC'))
+        torch.manual_seed(1712)
+        dataset = dataset.shuffle()
+
     elif name in ['CORA', 'CITESEER', 'PUBMED']:
         dataset = Planetoid(root=os.path.join(path, 'Planetoid'), name=name)
-        data = dataset[0]  # only one graph
-        train_loader = test_loader = data  # use the same for node-level
-        task_type = 'node'
+        data = dataset[0]
+        return dataset, data, data, 'node'
+
+    elif name in ['CORNELL', 'WISCONSIN']:
+        dataset = WikipediaNetwork(root=os.path.join(path, 'WebKB'), name=name.lower(), geom_gcn_preprocess=True)
+        data = dataset[0]
+        return dataset, data, data, 'node'
+
     else:
         raise ValueError(f"Dataset '{name}' not supported.")
-    
+
+    if train_size and test_size:
+        train_dataset = dataset[:train_size]
+        test_dataset = dataset[train_size:train_size + test_size]
+    else:
+        train_dataset = dataset[:int(0.8 * len(dataset))]
+        test_dataset = dataset[int(0.8 * len(dataset)):]
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
     return dataset, train_loader, test_loader, task_type
+
 
 def eval_dataset(name, path='../data', eval_size=None, batch_size=32, seed=1309):
     name = name.upper()
-    
-    if name in ['MUTAG', 'ENZYMES', 'PROTEINS']:
+    task_type = 'graph'
+
+    if name in ['MUTAG', 'ENZYMES', 'PROTEINS', 'REDDIT-BINARY']:
         dataset = TUDataset(os.path.join(path, 'TUDataset'), name=name)
         torch.manual_seed(seed)
         dataset = dataset.shuffle()
-        if eval_size:
-            eval_set = dataset[:eval_size]
-        else:
-            eval_set = dataset[int(0.8 * len(dataset)):]
+        eval_set = dataset[:eval_size] if eval_size else dataset[int(0.8 * len(dataset)):]
         eval_loader = DataLoader(eval_set, batch_size=batch_size)
-        task_type = 'graph'
+
+    elif name == 'ZINC':
+        dataset = ZINC(root=os.path.join(path, 'ZINC'))
+        torch.manual_seed(seed)
+        dataset = dataset.shuffle()
+        eval_set = dataset[:eval_size] if eval_size else dataset[int(0.8 * len(dataset)):]
+        eval_loader = DataLoader(eval_set, batch_size=batch_size)
+
     elif name in ['CORA', 'CITESEER', 'PUBMED']:
         dataset = Planetoid(root=os.path.join(path, 'Planetoid'), name=name)
-        data = dataset[0]  # only one graph
-        eval_loader = data  # use the same for node-level
+        eval_loader = dataset[0]
         task_type = 'node'
+
+    elif name in ['CORNELL', 'WISCONSIN']:
+        dataset = WikipediaNetwork(root=os.path.join(path, 'WebKB'), name=name.lower(), geom_gcn_preprocess=True)
+        eval_loader = dataset[0]
+        task_type = 'node'
+
     else:
         raise ValueError(f"Dataset '{name}' not supported.")
-    
+
     return eval_loader
 
 def random_split(data, train_ratio=0.6, val_ratio=0.2, seed=42):
